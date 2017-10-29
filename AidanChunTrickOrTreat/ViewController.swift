addb3d039989e1da812086aa652e0ba6228d670e
iwantmysleep <32229455+iwantmysleep@users.noreply.github.com>
2017-10-28 15:01:50 -1000

    Finish set up for view controller one

--- AidanChunTrickOrTreat/ViewController.swift
+++ AidanChunTrickOrTreat/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var knocKKnockButton: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView.image=UIImage(named: "doorImage")
+        knocKKnockButton.setTitle("knocKKnock", for:[])
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
